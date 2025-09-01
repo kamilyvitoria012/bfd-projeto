4c47856c5a7933f15b5aaa9ef20a62216a702cb2
Kamily vitoria <kamily0012@gmail.com>
2025-09-01 20:21:18 -0300

    Update consumo_agua.py
    removendo imc

--- consumo_agua.py
+++ consumo_agua.py
@@ -6,7 +6,6 @@ def sistema_de_agua():
     whatsapp = (input("Digite seu número de whatsapp:"))
 
 #Processamento
-    imc = peso/(altura**2)
     agua_ml = peso *35
     agua_l =agua_ml / 1000
 
@@ -14,15 +13,14 @@ def sistema_de_agua():
     print(f"Seu nome é: {nome}")
     print(f"Seu peso é: {peso} Kg")
     print(f"Sua altura é: {altura} m")
-    print(f"Seu IMC é: {imc:.2f}")
     print(f"Seu número de WhatsApp é: {whatsapp}")
     print(f"Você deve beber: {agua_l:.2f} litros de água por dia")
 
 
 #Menu Principal
 while True:
-        print ("\n ---------- Sistema de consumo de água ----------")
-        print (" Digite 1 para calcular aguá e IMC ")
+        print ("\n -------------- Sistema de consumo de água-------- ----------")
+        print (" Digite 1 para calcular seu consumo de água ")
         print (" Digite 2 para sair do sistema")
         opcao = input ("Escolha uma opção:")
         
